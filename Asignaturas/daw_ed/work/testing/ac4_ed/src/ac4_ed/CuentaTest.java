581f192cc76b80e35e7496f4ba88f7e70b0688e4
Sidney Silva <67187745+devsidney@users.noreply.github.com>
2023-04-27 17:21:23 +0200

    .json Base de Datos

--- Asignaturas/daw_ed/work/testing/ac4_ed/src/ac4_ed/CuentaTest.java
+++ Asignaturas/daw_ed/work/testing/ac4_ed/src/ac4_ed/CuentaTest.java
@@ -2,36 +2,44 @@ package ac4_ed;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class CuentaTest {
 
 	Cuenta c1 = new Cuenta();
+	Cuenta c2 = new Cuenta();
 
 	@Test
 	void testDeposito() {
 		// ingresar en el deposito
+		System.out.println(c1.Balance());
 		c1.Deposito(100);
+		System.out.println(c1.Balance());
+		assertEquals(100, c1.Balance(), 0, "Error...");
 	}
 
 	@Test
 	void testRetiro() {
 		// retirar dinero
+		c1.Deposito(100);
+		System.out.println(c1.Balance());
 		c1.Retiro(50);
+		System.out.println(c1.Balance());
+		assertEquals(50, c1.Balance(), 0, "Error...");
 	}
 
 	@Test
 	void testTransferencia() {
-		// transferir a una cuenta
-		c1.Transferencia(c1, 2000);
-		assertEquals();
+		// transferir de c1 a c2
+		System.out.println(c1.Balance());
+		c1.Transferencia(c2, 25);
+		System.out.println(c2.Balance());
+		assertEquals(25, c2.Balance(), 0, "Error...");
 	}
 
 	
-	void testBalance() {
-		// return de balance
-		c1.Balance();
-	}
+	
 
 }
+
+
