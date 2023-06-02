13723053658f55159a38fe71195766451a7027e8
Sidney Silva <67187745+sxdny@users.noreply.github.com>
2023-06-02 19:36:32 +0200

    aros

---
+++ Asignaturas/p/work/tema11/actividad11_nsque/Alumno.java
@@ -0,0 +1,22 @@
+import java.io.Serializable;
+
+public class Alumno extends Persona implements Serializable  {
+
+    private String curso = new String();
+    
+    public Alumno() {
+
+    }
+
+    public Alumno(String nombre, String apellidos, int edadPersona, String curso) {
+        super(nombre, apellidos, edadPersona);
+        this.curso = curso;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Alumno [curso=" + curso + "]";
+    }
+
+    
+}
--- Asignaturas/p/work/tema6/actividad6_5project/src/Persona.java
+++ Asignaturas/p/work/tema6/actividad6_5project/src/Persona.java
@@ -45,6 +45,4 @@ public class Persona {
         this.edadPersona = edadPersona;
     }
 
-    
-
 }
